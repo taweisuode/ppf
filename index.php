316e11e39f6c1399c6e7cb5729c479e35df2afc5
Pengge Zhuang <1064480036@qq.com>
2017-04-20 10:57:14 +0800

    refactor:重新改变Sys内系统文件的存储位置

--- index.php
+++ index.php
@@ -9,16 +9,15 @@ $path = str_replace(DIRECTORY_SEPARATOR,'/',dirname(__FILE__));
 define("PPF_PATH",$path);
 require_once(PPF_PATH.'/Application/Config/Config.php');
 require_once(PPF_PATH.'/Application/Config/Database.php');
-$allFile = scandir(PPF_PATH.'/Library/');
+$allFile = scandir(PPF_PATH.'/Library/Sys/');
 array_splice($allFile,0,2);//去掉前面的 '.' 和 '..'
 //获取文件夹的所有文件
-require_once(PPF_PATH.'/Library/Sys/Db_Table_Abstract.php');
 foreach($allFile as $key => $val)
 {   
     if(pathinfo($val,PATHINFO_EXTENSION) == 'php')
     {   
-        //加载Library下面的所有文件
-       require_once(PPF_PATH.'/Library/'.$val); 
+        //加载Library/Sys下面的所有文件
+       require_once(PPF_PATH.'/Library/Sys/'.$val);
     }   
 }   
 //初始化路由分发 根据request_uri来分发到各个控制器方法
