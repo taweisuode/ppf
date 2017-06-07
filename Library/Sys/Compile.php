48818ca215fb5220230fff97b2b533c12e21b0a3
Pengge Zhuang <1064480036@qq.com>
2017-06-07 16:07:11 +0800

    fix:foreach模版修改成无限递归遍历

--- Library/Sys/Compile.php
+++ Library/Sys/Compile.php
@@ -11,6 +11,9 @@ class Compile
     public $compare_destpattern = array();
     public function __CONSTRUCT()
     {
+        //添加include 模版
+        $this->compare_pattern[] = '#\{include (.*?)\}#';
+
         //简单的key value赋值
         $this->compare_pattern[] = '#\{\\$(.*?)\}#';
         //if条件语句实现
@@ -19,17 +22,18 @@ class Compile
         $this->compare_pattern[] = '#\{else(.*?)\}#';
         $this->compare_pattern[] = '#\{/if\}#';
         //foreach实现
-        $this->compare_pattern[] = '#\{foreach name=\\$(.*?)\}#';
-        $this->compare_pattern[] = '#\{\\$(key|val).\}#';
+        $this->compare_pattern[] = '#\{foreach name=\\$(.*?) item=(.*?) value=(.*?)\}#';
+        $this->compare_pattern[] = '#\{\\$(.*?)\}#';
         $this->compare_pattern[] = '#\{/foreach\}#';
         //支持原生php语言实现
         $this->compare_pattern[] = '#\{php (.*?)\}#';
         $this->compare_pattern[] = '#\{/php\}#';
 
-         //添加include 模版
-         $this->compare_pattern[] = '#\{include (.*?)\}#';
 
         //以下是上面几个模版编译后的php语言实现
+
+        $this->compare_destpattern[] = '<?php include "'.APPLICATION_PATH.'/'.Dispath::$current_module.'/View/'.'\\1";?>';
+
         $this->compare_destpattern[] = "<?php echo $\\1;?>";
 
         $this->compare_destpattern[] = "<?php if(\\1){ ?>";
@@ -37,21 +41,20 @@ class Compile
         $this->compare_destpattern[] = "<?php }else{ ?>";
         $this->compare_destpattern[] = "<?php }?>";
 
-        $this->compare_destpattern[] = "<?php foreach(\$\\1 as \$key => \$val){?>";
+        $this->compare_destpattern[] = "<?php foreach(\$\\1 as \$\\2 => \$\\3){?>";
         $this->compare_destpattern[] = '<?php echo $\\1; ?>';
         $this->compare_destpattern[] = "<?php } ?>";
 
         $this->compare_destpattern[] = "<?php \\1 ";
         $this->compare_destpattern[] = "?>";
 
-        $this->compare_destpattern[] = '<?php include "'.APPLICATION_PATH.'/'.Dispath::$current_module.'/View/'.'\\1";?>';
     }
     /**
      *   基本的编译功能实现 讲视图文件通过正则匹配编译并写入到php文件中
      *
      */
     public function compile($pre_compile_file,$dest_compile_file)
-    {    
+    {
         //echo $dest_compile_file;die;
         $compile_content = preg_replace($this->compare_pattern,$this->compare_destpattern,file_get_contents($pre_compile_file));
         file_put_contents($dest_compile_file,$compile_content);
