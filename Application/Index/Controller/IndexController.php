59553fbe02ff44fecb2246904a1451b099e49fb3
Pengge Zhuang <1064480036@qq.com>
2017-06-29 03:24:58 +0800

    feature:完善模版编译策略

--- Application/Index/Controller/IndexController.php
+++ Application/Index/Controller/IndexController.php
@@ -1,34 +1,32 @@
 <?php
-    class IndexController extends Controller {
-        public function indexAction() {
-            $this->load('Common/Function');
-            $str = "hello_aaa.html";
-            daddslashes($str,1);
-            
-            $this->view->assign("result","hello world!");
-            $this->view->show();
-        }
-        public function addAction() {
-            $indexModel = new IndexModel();
-            $movie_list = $indexModel->test();
-
-            $fruit = array("loving"=>'banana',"hating"=>'apple',"no_sense"=>'orange');
-            $test = array(
-                "aaa" => array(
-                    "yes" => "no",
-                    "sad" => 'happy'
-                ),
-                "bbb" => array(
-                    "one" => "two",
-                    "three"=> "four"
-                )
-            );
-            $this->view->assign("movie_list",$movie_list);
-            $this->view->assign("fruit",$fruit);
-            $this->view->assign("test",$test);
-            $this->view->assign("result","hello");
-            $this->view->show();
-        }
-
+class IndexController extends Controller {
+    public function indexAction() {
+        $indexModel = new IndexModel();
+        $list = $indexModel->getIndexList();
+        $this->view->assign("list",$list);
+        $this->view->show();
     }
+    public function addAction() {
+        $indexModel = new IndexModel();
+        $movie_list = $indexModel->test();
+
+        $fruit = array("loving"=>'aaaa',"hating"=>'apple',"no_sense"=>'orange');
+        $test = array(
+            "aaa" => array(
+                "yes" => "no",
+                "sad" => 'happy'
+            ),
+            "bbb" => array(
+                "one" => "two",
+                "three"=> "four"
+            )
+        );
+        $this->view->assign("movie_list",$movie_list);
+        $this->view->assign("fruit",$fruit);
+        $this->view->assign("test",$test);
+        $this->view->assign("result","hello");
+        $this->view->set_compile("111");
+        $this->view->show();
+    }
+}
 ?>
