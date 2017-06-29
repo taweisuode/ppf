475575c542a342964daa0d40d1c8bb9e5819fd01
Pengge Zhuang <1064480036@qq.com>
2017-06-29 14:09:43 +0800

    fix:修改bug

--- Application/Index/Controller/IndexController.php
+++ Application/Index/Controller/IndexController.php
@@ -1,31 +1,7 @@
 <?php
 class IndexController extends Controller {
     public function indexAction() {
-        $indexModel = new IndexModel();
-        $list = $indexModel->getIndexList();
-        $this->view->assign("list",$list);
-        $this->view->show();
-    }
-    public function addAction() {
-        $indexModel = new IndexModel();
-        $movie_list = $indexModel->test();
-
-        $fruit = array("loving"=>'aaaa',"hating"=>'apple',"no_sense"=>'orange');
-        $test = array(
-            "aaa" => array(
-                "yes" => "no",
-                "sad" => 'happy'
-            ),
-            "bbb" => array(
-                "one" => "two",
-                "three"=> "four"
-            )
-        );
-        $this->view->assign("movie_list",$movie_list);
-        $this->view->assign("fruit",$fruit);
-        $this->view->assign("test",$test);
-        $this->view->assign("result","hello");
-        $this->view->set_compile("111");
+        $this->view->assign("test","hello world!");
         $this->view->show();
     }
 }
--- Library/Sys/Compile.php
+++ Library/Sys/Compile.php
@@ -1,170 +1,98 @@
 <?php
-
 /**
- *  模版引擎基类Template 主要方法有以下
- *  对配置文件的设置
- *  对缓存策略的设计
- *  对assign 赋值的方法构造
- *  对show 方法的构造
- *  构造函数中加载编译类Compile
+ *  编译类  Compile
+ *  正则匹配式可拓展  可设定多个编译模版
+ *
  */
-
-class Template
+class Compile
 {
-    protected $compile;
-    public $value = array();
     public $config = array(
         'compiledir' => 'Cache/',         //设置编译后存放的目录
-        'need_compile' => true,           //是否需要重新编译 true 需要重新编译 false 直接加载缓存文件
         'suffix_cache' => '.htm',         //设置编译文件的后缀
-        'cache_time' => 2000              //多长时间自动更新，单位秒  
     );
+    public $value = array();
+    public $compare_pattern = array();
+    public $compare_destpattern = array();
+    public $compare_include_pattern = "";
+    public function __CONSTRUCT()
+    {
+        //添加include 模版
+        $this->compare_pattern[] = '#\{include (.*?)\}#';
 
-    /**
-     *   构造函数实例化编译类Compile
-     *
-     */
-    public function __CONSTRUCT() {
-        $compile = new Compile();
-        $this->compile = $compile;
+        //简单的key value赋值
+        $this->compare_pattern[] = '#\{\\$(.*?)\}#';
+        //if条件语句实现
+        $this->compare_pattern[] = '#\{if (.*?)\}#';
+        $this->compare_pattern[] = '#\{elseif(.*?)\}#';
+        $this->compare_pattern[] = '#\{else(.*?)\}#';
+        $this->compare_pattern[] = '#\{/if\}#';
+        //foreach实现
+        $this->compare_pattern[] = '#\{foreach name=\\$(.*?) item=(.*?) value=(.*?)\}#';
+        $this->compare_pattern[] = '#\{\\$(.*?)\}#';
+        $this->compare_pattern[] = '#\{/foreach\}#';
+        //支持原生php语言实现
+        $this->compare_pattern[] = '#\{php (.*?)\}#';
+        $this->compare_pattern[] = '#\{/php\}#';
+
+        $this->compare_pattern[] = '#\{compile_include (.*?)\}#';
+
+
+        //以下是上面几个模版编译后的php语言实现
+
+        $this->compare_destpattern[] = "<?php include PPF_PATH.'/'.\$this->config['compiledir'].".Dispath::$current_module.".'/'.md5('".Dispath::$current_controller.'_'.Dispath::$current_action.'_'."\\1').'.php'; ?>";
+
+        $this->compare_destpattern[] = "<?php echo $\\1;?>";
+
+        $this->compare_destpattern[] = "<?php if(\\1){ ?>";
+        $this->compare_destpattern[] = "<?php }else if(\\1){ ?>";
+        $this->compare_destpattern[] = "<?php }else{ ?>";
+        $this->compare_destpattern[] = "<?php }?>";
+        $this->compare_destpattern[] = "<?php foreach(\$\\1 as \$\\2 => \$\\3){?>";
+        $this->compare_destpattern[] = '<?php echo $\\1; ?>';
+        $this->compare_destpattern[] = "<?php } ?>";
+
+        $this->compare_destpattern[] = "<?php \\1 ";
+        $this->compare_destpattern[] = "?>";
+
+        $this->compare_destpattern[] = '<?php include "'.APPLICATION_PATH.'/'.Dispath::$current_module.'/View/'.'\\1";?>';
+
+        $this->compare_include_pattern = '#\{include (.*?)\}#';
     }
     /**
-     *   设置是否缓存(给控制器调用)
+     *   基本的编译功能实现 讲视图文件通过正则匹配编译并写入到php文件中
      *
      */
-    public function set_compile($value) {
-        if(is_bool($value)) {
-            $this->config['need_compile'] = $value;
+    public function compile($pre_compile_file,$dest_compile_file)
+    {
+        $compile_content = preg_replace($this->compare_pattern,$this->compare_destpattern,file_get_contents($pre_compile_file));
+        file_put_contents($dest_compile_file,$compile_content);
+    }
+    /**
+     * @desc 这块内容是先将include编译，然后生成在对应cache目录下的php文件，
+     * 将数组回传给Template.php然后在Template.php进行编译
+     *
+     */
+    public function match_include_file($file) {
+        $matchArr = array();
+        $match_file = preg_match_all($this->compare_include_pattern,file_get_contents($file),$matchArr);
+        if($match_file && !empty($matchArr[1])) {
+            $include_file_arr = array();
+            foreach($matchArr[1] as $key => $val) {
+                $compile_file_path = $this->get_compile_file_path();
+                $destpatternFile = APPLICATION_PATH."/".Dispath::$current_module."/View/".$val;
+                $compile_content = preg_replace($this->compare_pattern,$this->compare_destpattern,file_get_contents($destpatternFile));
+                $compile_file = $compile_file_path . md5(Dispath::$current_controller . '_' . Dispath::$current_action.'_'.$val) . '.php';
+                file_put_contents($compile_file,$compile_content);
+                $include_file_arr[] = $compile_file;
+            }
+            return $include_file_arr;
         }else {
-            throw new FrameException("设置缓存编译参数不正确",0);
+            return false;
         }
     }
-
-    public function set_config($key, $value) {
-        if (array_key_exists($this->config)) {
-            $this->config[$key] = $value;
-        }
-    }
-
-    public function get_config($key) {
-        if (array_key_exists($this->config)) {
-            return $this->config[$key];
-        }
-    }
-
-    /**
-     *   缓存策略
-     *   根据need_compile 是否需要重新编译
-     *   以及当前时间比该文件编译时间是否大于自动更新cache_time时间
-     *   以上2点来决定是需要再次编译还是直接使用缓存文件
-     *  @param  string $html_file 视图文件
-     *  @param  string $compile_file 编译文件
-     *  @return bool   $default_status 是否需要重新编译
-     */
-    public function cache_strategy($html_file, $compile_file) {
-        $default_status = false;
-        if(file_exists($compile_file)) {
-            $compile_file_time = filemtime($compile_file);
-        }
-        $time_minus = time() - $compile_file_time;
-        if (($this->config['need_compile']) || ($time_minus > $this->config['cache_time']) || filemtime($compile_file) < filemtime($html_file)) {
-            $default_status = true;
-        } else {
-            $default_status = false;
-        }
-        return $default_status;
-    }
-
-    /**
-     *  将变量赋值到$this->vaule中
-     *  @param $key
-     *  @param $value
-     */
-    public function assign($key, $value) {
-        $this->value[$key] = $value;
-    }
-
-    /**
-     *   视图跳转方法（包含了模版引擎，模版编译转化功能）
-     *   @param  $file  视图跳转文件
-     *
-     */
-    public function show($file = null) {
-        /**
-         *  将例如assign("test","aaa") 转化成 $test = 'aaa';
-         *  所以这块是有2个赋值情况  一个是$test = 'aaa' 另一个是 $this->value['test'] = 'aaa';
-         *  这里设定 可以支持多维数组传递赋值
-         *  @param string $file 视图文件
-         */
-        $session = Controller::$session;
-        foreach ($this->value as $key => $val) {
-            $$key = $val;
-        }
-        $current_module = Dispath::$current_module;
-        $current_controller = Dispath::$current_controller;
-        $compile_file_path = PPF_PATH . '/' . $this->config['compiledir'] . $current_module . '/';
-        /**
-         *   如果未指定视图名称则默认跳至该current_action的名称
-         *   在这块定义视图地址，编译php文件地址，缓存htm文件地址
-         */
-        if (!$file) {
-            $current_action = Dispath::$current_action;
-            $html_file = APPLICATION_PATH . '/' . $current_module . '/View/' . $current_controller . '/' . $current_action . '.html';
-            $compile_file = $compile_file_path . md5($current_controller . '_' . $current_action) . '.php';
-            $cache_file = $compile_file_path . md5($current_controller . '_' . $current_action) . $this->config['suffix_cache'];
-        } else {
-            $html_file = APPLICATION_PATH . '/' . $current_module . '/View/' . $current_controller . '/' . $file . '.html';
-            $compile_file = $compile_file_path . md5($current_controller . '_' . $file) . '.php';
-            $cache_file = $compile_file_path . md5($current_controller . '_' . $file) . $this->config['suffix_cache'];
-        }
-        /**
-         *   如果存在视图文件html_file  则继续根据条件编译，否则跳至/Index/view/Notfound/index.html
-         */
-        if (is_file($html_file)) {
-            /**
-             *   对compile_file_path进行是否为路径的判断 如果不是 则进行创建并赋予755的权限
-             */
-            if (!is_dir($compile_file_path)) {
-                mkdir($compile_file_path);
-                //chmod($compile_file_path, 0755);
-            }
-            /**
-             *   这3行代码是将Controller.php文件某一方法例如：$this->assign("add",'test')；
-             *   将这个以键值对的方式传给在__CONSTRUCT实例化的Compile类中，并通过compile方法进行翻译成php文件
-             *   最后ob_start()方法需要  include $compile_file;
-             */
-            if ($this->cache_strategy($html_file, $compile_file)) {
-                $this->compile->value = $this->value;
-
-                /**
-                 * @desc 这里是先编译include部分的内容，然后在全部编译完毕
-                 */
-                ob_start();
-                $this->compile->match_include_file($html_file);
-                $this->compile->compile($html_file, $compile_file);
-                include "$compile_file";
-                /**
-                 *   这块是得到输出缓冲区的内容并将其写入缓存文件$cache_file中，同时将编译文件跟缓存文件进行赋予755权限
-                 *   这时可以去看看Cache下面会有2个文件 一个是php文件 一个是htm文件 htm文件就是翻译成html语言的缓存文件
-                 */
-                $message = ob_get_contents();
-                /**
-                if(file_exists($compile_file)) {
-                chmod($compile_file, 0777);
-                }
-                if(file_exists($cache_file)) {
-                chmod($cache_file, 0777);
-                }
-                 */
-                $file_line = file_put_contents($cache_file, $message);
-                ob_end_flush();
-            } else {
-                include "$cache_file";
-            }
-        } else {
-            include APPLICATION_PATH . '/Index/View/Notfound/index.html';
-        }
+    private function get_compile_file_path() {
+        $compile_file_path = PPF_PATH . '/' . $this->config['compiledir'] . Dispath::$current_module . '/';
+        return $compile_file_path;
     }
 }
-
 ?>
